a0c3a9d18e505d1d47a9f29863ae1ccc7c172498
GoAlndr <90776548+GoAlndr@users.noreply.github.com>
2021-10-23 21:37:12 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+class Mus:
+    def __init__(self, id, fio, sal, orch_id):
+        self.id = id
+        self.fio = fio
+        self.sal = sal
+        self.orc_id = orch_id
+
+class Orch:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+mus = []
+orch = []
+
+class 
