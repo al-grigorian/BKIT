c9c4822e5ffa6e5dac87df588bdf2f5d773a5f4b
grSashha <90776548+grSashha@users.noreply.github.com>
2021-10-24 20:22:22 +0300

    Revert "Update main.py"
    This reverts commit 14d703db1abeef362836a031521e8e8c82ab7905.

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from copy import deepcopy
-import operator
-
 class Mus:
     def __init__(self, id, fio, sal, orch_id):
         self.id = id
@@ -49,14 +46,12 @@ mus_orch = [
 ]
 
 def main():
-    # соединение данных один-ко-многим
     one_to_many = [(o.name, m.fio, m.sal) 
         for o in orch
         for m in mus
         if m.orc_id == o.id
     ]
 
-    # соединение данных многие-ко-многим
     many_to_many_temp = [(o.name, m_o.orch_id, m_o.mus_id) 
         for o in orch 
         for m_o in mus_orch
@@ -66,70 +61,25 @@ def main():
         for orch_name, orch_id, mus_id in many_to_many_temp
         for m in mus if m.id == mus_id]
 
-    #------------------------Задание Г1-------------------------------------------
-    res1 = {}
-    for o in orch:
-        for elem in one_to_many:
-            if o.name == elem[0]:
-                
-        
-
-    #-----------------------------------------------------------------------------
-
-
-    #------------------------Задание Г2-------------------------------------------
-    # словарь для оркестров, названия которых начинаются с буквы 'А'
-    res2 = {}
-    # зарплаты
-    sal = []
-    for o in orch:
-        for elem in one_to_many:
-            if o.name == elem[0]:
-                # добавление значения зарплаты для каждого музыканта каждого оркестра
-                sal.append(elem[2])
-        # добавление списка зарплат для каждого оркестра
-        res2[o.name] = deepcopy(sal)
-        # очищение списка для зарплат нового оркестра
-        sal.clear()
-
-    # поиск максимальной зарплаты в каждом отделе
-    for value in res2.values():
-        if len(value) != 0:
-            m = max(value)
-            value.clear()
-            value.append(m)
-    print("Задание Г2:")
-    print(res2)
-    #-------------------------------------------------------------------
-    
-
-    #--------------------------Задание Г3-------------------------------
-
-
-
-
-
-
-    #-------------------------------------------------------------------
-
-    """
+    d_orch_mus = {}
     for elem in one_to_many:
         if elem[0][0] == 'А':
-            # добавление нового оркестра (ключ - название, значение - фа)
-            d_orch_mus[elem[0]] = []
-    """
+            d_orch_mus[elem[0]] = elem[1]
     
-    
-    """
+    print("Задание Г1:")
+    print(d_orch_mus)
+
     m = 0
     res = []
     for o in orch:
         for elem in one_to_many:
-            if elem[0]
-
+            if o.name == elem[0]:
                 if elem[2] > m:
                     m = elem[2]
-    """
+                    
+
+    print("Задание Г2")
+    print("Задание Г3")
 
 if __name__ == '__main__':
     main()
